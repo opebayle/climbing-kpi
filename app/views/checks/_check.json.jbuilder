af734211af769cc58843a71703e8a496e3c79625
opebayle <opebayle@gmail.com>
2017-04-24 21:21:32 +0200

    Generation de Check, commande : rails generate scaffold Check route_id:integer user_id:integer

---
+++ app/views/checks/_check.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! check, :id, :route_id, :user_id, :created_at, :updated_at
+json.url check_url(check, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :checks
   resources :levels
   resources :grades
   resources :routes
