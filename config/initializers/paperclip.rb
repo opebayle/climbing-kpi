2640420b5827869dd402ff44f8b6e3a57c7d7eda
opebayle <opebayle@gmail.com>
2018-06-05 21:57:58 +0200

    ajout config S3 paperclip

---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,10 @@
+
+
+# config/initializers/paperclip.rb
+Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+
+# config/initializers/paperclip.rb
+Paperclip::Attachment.default_options[:s3_host_name] = 's3-eu-west-3.amazonaws.com'
+
+
