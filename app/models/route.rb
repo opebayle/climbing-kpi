8e479ec3af014251fed9bf3115e27374976a5c2a
opebayle <opebayle@gmail.com>
2018-06-08 22:43:41 +0200

    correction missing picture

--- app/models/route.rb
+++ app/models/route.rb
@@ -4,7 +4,7 @@ class Route < ActiveRecord::Base
   has_many   :checks
   has_many   :users, :through => :checks
   before_save :default_values
-  has_attached_file :image, styles: { medium: "200x200>", thumb: "50x50#", default_url:"missing.png"  }
+  has_attached_file :image, styles: { medium: "200x200>", thumb: "50x50#", default_url:"/images/missing.png"  }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
   def default_values
